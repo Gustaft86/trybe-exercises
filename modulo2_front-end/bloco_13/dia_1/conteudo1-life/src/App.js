37226e2b448f12d04343835e9112240ebbeb4d85
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-01-25 00:47:44 -0300

    conteudo e exercicios do bloco 13

--- modulo2_front-end/bloco_13/dia_1/conteudo1-life/src/App.js
+++ modulo2_front-end/bloco_13/dia_1/conteudo1-life/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CounterDisplay from './Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App.js
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Contador
+        <button
+          onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}>
+          Soma
+        </button>
+        <CounterDisplay value={this.state.counter} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ modulo2_front-end/bloco_13/dia_1/dadjoke-2.0/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import DadJoke from './DadJoke';
+
+function App() {
+  return (
+    <div className="App">
+      <DadJoke />
+    </div>
+  );
+}
+
+export default App;
